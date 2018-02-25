9ff4c97b23621525f004cf6966b49ea164022c44
unknown <brittanycturner@gmail.com>
2018-02-25 10:02:20 -0700

    almost there...

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,17 +8,18 @@ var userNumber = 0;
 var numberOfWins = 0;
 var numberOfLosses = 0;
 
+    
 
 // Global Functions
 function checkForWin () {
     if (userNumber === winningNumber) {
-        console.log ("you win!");
+        console.log ("you won!");
         numberOfWins++;
         $("#wins").text(numberOfWins);
         resetGame();
 
     } else if (userNumber > winningNumber) {
-        console.log ("you loss!");
+        console.log ("you lost!");
         numberOfLosses++;
         $("#losses").text(numberOfLosses);
         resetGame()
@@ -32,9 +33,69 @@ function getRandomNumber() {
 
 function resetGame() {
     userNumber = 0;
-
     $("#user-number").text(userNumber);
+
     winningNumber = Math.floor((Math.random() * 120) + 19);
+    $("#winning-number").text(winningNumber);
+    console.log(winningNumber);
+
+    crystal1 = getRandomNumber();
+    console.log ("crystal1: "+crystal1); 
+    crystal2 = getRandomNumber();
+    console.log ("crystal2: "+crystal2);
+    crystal3 = getRandomNumber();
+    console.log ("crystal3: "+crystal3);
+    crystal4 = getRandomNumber();
+    console.log ("crystal4: "+crystal4);
+
+}
+
+
+// when you click on a crystal, give it the value of a random number
+
+//     function clickCrystal (value, id) {
+//     $("#"+id).click(function(event){
+//         userNumber = value + userNumber;
+//         console.log(value);
+//         $("#user-number").text(userNumber);
+//         checkForWin();
+//     });
+// }
+
+$("#crystal1").click(function(event){
+    userNumber = crystal1 + userNumber;
+    $("#user-number").text(userNumber);
+    console.log(crystal1);
+    checkForWin();
+});
+
+$("#crystal2").click(function(event){
+    userNumber = crystal2 + userNumber;
+    $("#user-number").text(userNumber);
+    console.log(crystal2);
+    checkForWin();
+});
+
+$("#crystal3").click(function(event){
+    userNumber = crystal3 + userNumber;
+    $("#user-number").text(userNumber);
+    console.log(crystal3);
+    checkForWin();
+});
+
+$("#crystal4").click(function(event){
+    userNumber = crystal4 + userNumber;
+    $("#user-number").text(userNumber);
+    console.log(crystal4);
+    checkForWin();
+});
+
+
+
+// pick a random number for each crystal & for the winning number
+ $(document).ready(function() {
+    $("#user-number").text(userNumber);
+    winningNumber = Math.floor((Math.random() * 120) + 19); //need to look at it being inclusive
     $("#winning-number").text(winningNumber);
     console.log(winningNumber);
 
@@ -47,37 +108,13 @@ function resetGame() {
     crystal4 = getRandomNumber();
     console.log ("crystal4: "+crystal4);
     
-}
-
-// pick a random number for each crystal & for the winning number
- $(document).ready(function() {
-    $("#user-number").text(userNumber);
-    winningNumber = Math.floor((Math.random() * 120) + 19);
-    $("#winning-number").text(winningNumber);
-    console.log(winningNumber);
-
-    crystal1 = getRandomNumber();
-    console.log ("crystal1: "+crystal1);
-    crystal2 = getRandomNumber();
-    console.log ("crystal2: "+crystal2);
-    crystal3 = getRandomNumber();
-    console.log ("crystal3: "+crystal3);
-    crystal4 = getRandomNumber();
-    console.log ("crystal4: "+crystal4);
-
-    clickCrystal(crystal1, "crystal1");
-    clickCrystal(crystal2, "crystal2");
-    clickCrystal(crystal3, "crystal3");
-    clickCrystal(crystal4, "crystal4");
-}); 
-
-// when you click on a crystal, give it the value of a random number // Create a For Loop After?
-function clickCrystal (value, id) {
-    $("#"+id).click(function(event){
-        userNumber = value + userNumber;
-        $("#user-number").text(userNumber);
-        checkForWin();
-    });
-}
+  //  clickCrystal(crystal1, "crystal1");
+  //  clickCrystal(crystal2, "crystal2");
+  //  clickCrystal(crystal3, "crystal3");
+  //  clickCrystal(crystal4, "crystal4");
+    
+    }); 
 
 
+
+    
